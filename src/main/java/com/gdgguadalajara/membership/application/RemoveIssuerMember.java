8d190b346a0a469e977eab4a1272e1d466d245de
ushieru <uzielcocolan@gmail.com>
2026-01-28 14:56:33 -0600

    Refactor(RemoveIssuerMember): remove unused dependencies and authorization check

--- src/main/java/com/gdgguadalajara/membership/application/RemoveIssuerMember.java
+++ src/main/java/com/gdgguadalajara/membership/application/RemoveIssuerMember.java
@@ -2,8 +2,6 @@ package com.gdgguadalajara.membership.application;
 
 import java.util.UUID;
 
-import com.gdgguadalajara.authentication.application.GetCurrentSession;
-import com.gdgguadalajara.common.model.DomainException;
 import com.gdgguadalajara.membership.model.IssuerMember;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -14,12 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RemoveIssuerMember {
 
-    private final GetCurrentSession getCurrentSession;
-
     public void run(UUID issuerUuid, UUID accountUuid) {
-        var session = getCurrentSession.run();
-        if (!session.isSuperAdmin)
-            throw DomainException.forbidden("Solo super administradores pueden eliminar miembros de emisores");
         IssuerMember member = IssuerMember.find("issuer.id = ?1 and account.id = ?2", issuerUuid, accountUuid)
                 .firstResult();
         if (member == null)
