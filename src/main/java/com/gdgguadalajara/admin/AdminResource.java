9f880e7f4e08a0c4c089900045ca200cab4a3755
ushieru <uzielcocolan@gmail.com>
2026-01-29 12:31:02 -0600

    Feat(AdminResource): implement endpoints for retrieving assertions and improve error handling

--- src/main/java/com/gdgguadalajara/admin/AdminResource.java
+++ src/main/java/com/gdgguadalajara/admin/AdminResource.java
@@ -2,10 +2,19 @@ package com.gdgguadalajara.admin;
 
 import java.util.UUID;
 
+import com.gdgguadalajara.assertion.model.Assertion;
+import com.gdgguadalajara.authentication.application.GetCurrentSession;
 import com.gdgguadalajara.badgeclass.model.BadgeClass;
+import com.gdgguadalajara.common.PageBuilder;
 import com.gdgguadalajara.common.model.DomainException;
+import com.gdgguadalajara.common.model.PaginatedResponse;
+import com.gdgguadalajara.common.model.dto.PaginationRequestParams;
 import com.gdgguadalajara.issuer.model.Issuer;
+import com.gdgguadalajara.membership.model.IssuerMember;
 
+import io.quarkus.security.Authenticated;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import lombok.AllArgsConstructor;
@@ -13,6 +22,8 @@ import lombok.AllArgsConstructor;
 @Path("/api/admin")
 @AllArgsConstructor
 public class AdminResource {
+
+    private final GetCurrentSession getCurrentSession;
 
     @GET
     @Path("/issuers/{uuid}")
@@ -30,5 +41,34 @@ public class AdminResource {
         if (baadge == null)
             throw DomainException.notFound("Credencial no encontrada");
         return baadge;
+    }
+
+    @GET
+    @Path("/me/assertions")
+    @Authenticated
+    public PaginatedResponse<Assertion> readAssertions(@BeanParam @Valid PaginationRequestParams request) {
+        var account = getCurrentSession.run();
+        return PageBuilder.of(Assertion.find("account", account), request);
+    }
+
+    @GET
+    @Path("/assertions/{assertionUuid}")
+    @Authenticated
+    public Assertion readAssertionByUuid(UUID assertionUuid) {
+        var account = getCurrentSession.run();
+        var assertion = Assertion.<Assertion>findById(assertionUuid);
+        if (assertion == null)
+            throw DomainException.notFound("Acreditación no encontrada");
+        if (account.isSuperAdmin)
+            return assertion;
+        if (assertion.account.id.equals(account.id))
+            return assertion;
+        var counter = IssuerMember.find(
+                "account.id = ?1 and issuer.id = ?2",
+                account.id,
+                assertion.badgeClass.issuer.id).count();
+        if (counter > 0)
+            return assertion;
+        throw DomainException.notFound("Acreditación no encontrada");
     }
 }
